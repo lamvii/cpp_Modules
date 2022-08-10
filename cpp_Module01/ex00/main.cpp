0dfc7e893787b90acceb10f339108114380756f2
lamvii <ael-idri@e3r10p14.1337.ma>
2022-08-11 00:36:28 +0100

    fix

--- cpp_Module01/ex00/main.cpp
+++ cpp_Module01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-idri <ael-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 23:34:23 by ael-idri          #+#    #+#             */
-/*   Updated: 2022/08/11 00:12:36 by ael-idri         ###   ########.fr       */
+/*   Updated: 2022/08/11 00:23:34 by ael-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ int	main()
 	randomChump("random");
 	Z2->announce();
 	Z1.announce();
-	// system("leaks Zombie");
+	delete	Z2;
 }
