9662efcaaaf3c780ed7bfaca6e0a572f3afa8d81
lamvii <ael-idri@e3r10p14.1337.ma>
2022-08-11 01:12:40 +0100

    add comments

--- cpp_Module01/ex00/main.cpp
+++ cpp_Module01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-idri <ael-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 23:34:23 by ael-idri          #+#    #+#             */
-/*   Updated: 2022/08/11 00:23:34 by ael-idri         ###   ########.fr       */
+/*   Updated: 2022/08/11 00:49:11 by ael-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	main()
 {
 	Zombie	Z1;
 	Zombie	*Z2 = newZombie("hhh");
+	// Z2->announce();
 	randomChump("random");
 	Z2->announce();
 	Z1.announce();
--- cpp_Module01/ex00/newZombie.cpp
+++ cpp_Module01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-idri <ael-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 22:49:33 by ael-idri          #+#    #+#             */
-/*   Updated: 2022/08/11 00:13:13 by ael-idri         ###   ########.fr       */
+/*   Updated: 2022/08/11 00:43:13 by ael-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ Zombie* newZombie( std::string name )
 {
 	Zombie *Z = new ::Zombie();
 	// Zombie Z;
+	// Z.set_name;
+	// return (&Z);
 	(*Z).set_name(name);
 	return (Z);
 }
