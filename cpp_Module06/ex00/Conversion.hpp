9a83d7f0dec4399de7c6bbea7448907be9f6cbdd
lamvii <ael-idri@e3r12p9.1337.ma>
2022-09-14 20:26:09 +0100

    ex00 almost finished

--- cpp_Module06/ex00/Conversion.hpp
+++ cpp_Module06/ex00/Conversion.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-idri <ael-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 18:36:35 by ael-idri          #+#    #+#             */
-/*   Updated: 2022/09/13 22:46:41 by ael-idri         ###   ########.fr       */
+/*   Updated: 2022/09/14 20:21:49 by ael-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <sstream>
 #include <limits>
+#include <cmath>
 
 using std::cout;
 using std::endl;
@@ -81,92 +82,6 @@ void Conversion::_guide(void)
 	cout << "double literals: 0.0, -4.2, 4.2, nan, -inf, +inf..." <<endl;
 }
 
-void Conversion::parsing(string av)
-{
-	std::stringstream ss;
-	long c_long;
-	long double c_ldouble;
-	int i = 0;
-
-
-	if (!isdigit(av[0]) && av.length() == 1)
-	{
-		this->c_char = av[0];
-		cout << c_char << endl;
-	}
-	else if (!av.compare("nan"))
-	{
-		c_double = nan("");
-		cout << c_double;
-	}
-	else if (!av.compare("nanf"))
-	{
-		c_float = nanf("");
-		cout << c_float;
-	}
-	else if (!av.compare("inf") || !av.compare("+inf"))
-	{
-		c_double = std::numeric_limits<double>::infinity();
-		cout << c_double;
-	}
-	else if (!av.compare("-inf"))
-	{
-		c_double = -std::numeric_limits<double>::infinity();
-		cout << c_double;
-	}
-	else if (!av.compare("inff") || !av.compare("+inff"))
-	{
-		c_float = std::numeric_limits<float>::infinity();
-		cout << c_float;
-	}
-	else if (!av.compare("-inff"))
-	{
-		c_float = -std::numeric_limits<float>::infinity();
-		cout << c_float;
-	}
-	else if (av[i] == '+' || av[i] == '-' || isdigit(av[i]))
-	{
-		ss << av[i++];
-		for (; i < av.length() && isdigit(av[i]); i++)
-			ss << av[i];
-		if (!av[i])
-		{
-			ss >> c_long;
-			c_int = static_cast<int>(c_long);
-			if (c_long > std::numeric_limits<int>::max() || c_long < std::numeric_limits<int>::lowest())
-				flag[f_int] = Impossible;
-			cout << c_int << endl;
-		}
-		else if (av[i] == '.')
-		{
-			ss << av[i++];
-			for (; i < av.length() && isdigit(av[i]); i++)
-				ss << av[i];
-			if (!av[i])
-			{
-				ss >> c_ldouble;
-				c_double = static_cast<double>(c_ldouble);
-				if (c_ldouble > std::numeric_limits<double>::max() || c_ldouble < std::numeric_limits<double>::lowest())
-					flag[f_double] = Impossible;
-				cout << c_double << c_ldouble << endl;
-			}
-			else if ((av[i] == 'f' || av[i] == 'F') && !av[i + 1])
-			{
-				ss >> c_double;
-				c_float = static_cast<float>(c_double);
-				flag[f_float] = Printable;
-				cout << c_double << c_float << endl;
-			}
-			else
-				this->_guide();
-		}
-		else
-			this->_guide();
-	}
-	else
-		this->_guide();
-}
-
 void Conversion::convertFromChar()
 {
 	if (c_char > 32 && c_char < 127)
@@ -183,58 +98,217 @@ void Conversion::convertFromChar()
 
 void Conversion::convertFromInt()
 {
-	c_char = static_cast<char>(c_int);
-	if (c_int < 0 && c_int > 255)
-		flag[f_char] = Impossible;
-	else if (c_int > 32 && c_int < 127)
-		flag[f_char] = Printable;
+	if (flag[f_int] == Impossible)
+	{
+		flag[f_char] = flag[f_float] = flag[f_double] = flag[f_int];
+	}
 	else
-		flag[f_char] = NonPrintable;
-	c_float = static_cast<float>(c_int);
-		flag[f_float] = Printable;
-	c_double = static_cast<double>(c_int);
-		flag[f_double] = Printable;
-	// case int impossible
+	{
+		c_char = static_cast<char>(c_int);
+		if (c_int < 0 || c_int > 255)
+			flag[f_char] = Impossible;
+		else if (c_int > 32 && c_int < 127)
+			flag[f_char] = Printable;
+		else
+			flag[f_char] = NonPrintable;
+		c_float = static_cast<float>(c_int);
+			flag[f_float] = Printable;
+		c_double = static_cast<double>(c_int);
+			flag[f_double] = Printable;
+	}
+	// case int impossible --> char impos(non sense) --> float double impos(non sense)
 }
 
 void Conversion::convertFromFloat()
 {
-	c_char = static_cast<char>(c_float);
-	if (c_float < 0 && c_float >= 256)
-		flag[f_char] = Impossible;
-	else if (c_float >= 31 && c_float < 127)
-		flag[f_char] = Printable;
+	if (flag[f_float] == Impossible)
+	{
+		flag[f_char] = flag[f_int] = flag[f_double] = flag[f_float];
+	}
+	else if (c_float == std::numeric_limits<float>::infinity() || c_float == -std::numeric_limits<float>::infinity() || std::isnan(c_float))
+	{
+		flag[f_char] = flag[f_int] = Impossible;
+		c_double = static_cast<double>(c_float);
+			flag[f_double] = Printable;
+	}
 	else
-		flag[f_char] = NonPrintable;
-	c_int = static_cast<int>(c_float);
-	if (c_float >= std::numeric_limits<int>::max() + 1 || c_float < std::numeric_limits<int>::lowest())
-		flag[f_int] = Printable;
-	else 
-		flag[f_int] = Impossible;
-	c_double = static_cast<double>(c_float);
-		flag[f_double] = Printable;
+	{
+		c_char = static_cast<char>(c_float);
+		if (c_float < 0 || c_float >= 256)
+			flag[f_char] = Impossible;
+		else if (c_float >= 31 && c_float < 127)
+			flag[f_char] = Printable;
+		else
+			flag[f_char] = NonPrintable;
+		c_int = static_cast<int>(c_float);
+		if (c_float > (float)(INT_MAX) + 1 || c_float < INT_MIN)
+			flag[f_int] = Impossible;
+		else 
+			flag[f_int] = Printable;
+		c_double = static_cast<double>(c_float);
+			flag[f_double] = Printable;
+	}
 	// case float impossible nan -inf ..
 }
 
 void Conversion::convertFromDouble()
 {
-	c_char = static_cast<char>(c_double);
-	if (c_double < 0 && c_double >= 256)
-		flag[f_char] = Impossible;
-	else if (c_double >= 31 && c_double < 127)
-		flag[f_char] = Printable;
-	else
-		flag[f_char] = NonPrintable;
-	c_int = static_cast<int>(c_double);
-	if (c_float >= std::numeric_limits<int>::max() + 1 || c_float < std::numeric_limits<int>::lowest())
-		flag[f_int] = Printable;
-	else
-		flag[f_int] = Impossible;
-	c_float = static_cast<float>(c_double);
-	if (c_float >= std::numeric_limits<float>::max() + 1 || c_float < std::numeric_limits<float>::lowest())
+	if (flag[f_double] == Impossible)
+	{
+		flag[f_char] = flag[f_int] = flag[f_float] = flag[f_double];
+	}
+	else if (c_double == std::numeric_limits<double>::infinity() || c_double == -std::numeric_limits<double>::infinity() || std::isnan(c_double))
+	{
+		flag[f_char] = flag[f_int] = Impossible;
+		c_float = static_cast<double>(c_double);
 		flag[f_float] = Printable;
+	}
 	else
-		flag[f_float] = Impossible;
+	{
+		c_char = static_cast<char>(c_double);
+		if (c_double < 0 || c_double >= 256)
+			flag[f_char] = Impossible;
+		else if (c_double >= 31 && c_double < 127)
+			flag[f_char] = Printable;
+		else
+			flag[f_char] = NonPrintable;
+		c_int = static_cast<int>(c_double);
+		if (c_double > (float)(INT_MAX) + 1 || c_double < INT_MIN)
+			flag[f_int] = Impossible;
+		else
+			flag[f_int] = Printable;
+		c_float = static_cast<float>(c_double);
+		if (c_double > std::numeric_limits<float>::max() || c_double < -std::numeric_limits<float>::max())
+			flag[f_float] = Impossible;
+		else
+			flag[f_float] = Printable;
+	}
 }
 
-// display data :) 
+void Conversion::parsing(string av)
+{
+	std::stringstream ss;
+	long c_long;
+	long double c_ldouble;
+	int i = 0;
+
+
+	if (!isdigit(av[0]) && av.length() == 1)
+	{
+		this->c_char = av[0];
+		flag[f_char] = Printable;
+		this->convertFromChar();
+	}
+	else if (!av.compare("nan"))
+	{
+		c_double = std::numeric_limits<double>::quiet_NaN();
+		flag[f_double] = Printable;
+		this->convertFromDouble();
+	}
+	else if (!av.compare("nanf"))
+	{
+		c_float = std::numeric_limits<float>::quiet_NaN( );
+		flag[f_float] = Printable;
+		this->convertFromFloat();
+	}
+	else if (!av.compare("inf") || !av.compare("+inf"))
+	{
+		c_double = std::numeric_limits<double>::infinity();
+		flag[f_double] = Printable;
+		this->convertFromDouble();
+	}
+	else if (!av.compare("-inf"))
+	{
+		c_double = -std::numeric_limits<double>::infinity();
+		flag[f_double] = Printable;
+		this->convertFromDouble();
+	}
+	else if (!av.compare("inff") || !av.compare("+inff"))
+	{
+		c_float = std::numeric_limits<float>::infinity();
+		flag[f_float] = Printable;
+		this->convertFromFloat();
+	}
+	else if (!av.compare("-inff"))
+	{
+		c_float = -std::numeric_limits<float>::infinity();
+		flag[f_float] = Printable;
+		this->convertFromFloat();
+	}
+	else if (av[i] == '+' || av[i] == '-' || isdigit(av[i]))
+	{
+		ss << av[i++];
+		for (; i < av.length() && isdigit(av[i]); i++)
+			ss << av[i];
+		if (!av[i])
+		{
+			ss >> c_long;
+			c_int = static_cast<int>(c_long);
+			if (c_long > std::numeric_limits<int>::max() || c_long < INT_MIN)
+				flag[f_int] = Impossible;
+			else
+				flag[f_int] = Printable;
+			this->convertFromInt();
+		}
+		else if (av[i] == '.')
+		{
+			ss << av[i++];
+			for (; i < av.length() && isdigit(av[i]); i++)
+				ss << av[i];
+			if (!av[i])
+			{
+				ss >> c_ldouble;
+				c_double = static_cast<double>(c_ldouble);
+				if (c_ldouble > std::numeric_limits<double>::max() || c_ldouble < -std::numeric_limits<double>::max())
+					flag[f_double] = Impossible;
+				else
+					flag[f_double] = Printable;
+				this->convertFromDouble();
+			}
+			else if ((av[i] == 'f' || av[i] == 'F') && !av[i + 1])
+			{
+				ss >> c_double;
+				c_float = static_cast<float>(c_double);
+				if (c_double > std::numeric_limits<float>::max() || c_double < -std::numeric_limits<float>::max())
+					flag[f_float] = Impossible;
+				else
+					flag[f_float] = Printable;
+				this->convertFromFloat();
+			}
+			else
+				this->_guide();
+		}
+		else
+			this->_guide();
+	}
+	else
+		this->_guide();
+}
+
+void Conversion::display()
+{
+	if (flag[f_char] == Impossible)
+		cout << "char	:	Impossible" << endl;
+	else if (flag[f_char] == NonPrintable)
+		cout << "char	:	Non displayable" << endl;
+	else
+		cout << "char	:	" << c_char << endl;
+	if (flag[f_int] == Impossible)
+		cout << "int	:	Impossible" << endl; 
+	else
+		cout << "int	:	" << c_int << endl;
+	if (flag[f_float] == Impossible)
+		cout << "float	:	Impossible" << endl;
+	else if (std::isnan(c_float))
+		cout << "float	:	nanf" << endl;
+	else if (c_float == std::numeric_limits<float>::infinity())
+		cout << "float	:	inff" << endl;
+	else if (c_float == -std::numeric_limits<float>::infinity())
+		cout << "float	:	-inff" << endl;
+	else
+		cout << "float	:	" << c_float << endl;
+	if (flag[f_double] == Impossible)
+		cout << "double	:	Impossible" << endl;
+	else
+		cout << "double	:	" << c_double << endl;
+}
--- cpp_Module06/ex00/main.cpp
+++ cpp_Module06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-idri <ael-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 21:28:46 by ael-idri          #+#    #+#             */
-/*   Updated: 2022/09/13 22:27:26 by ael-idri         ###   ########.fr       */
+/*   Updated: 2022/09/14 20:22:36 by ael-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,13 @@
 
 int main(int ac, char **av)
 {
-    // Conversion conv;
+    Conversion conv;
 
-    // if (ac != 2)
-    // {
-    //     conv._guide();
-    //     return (1);
-    // }
-    // conv.parsing(av[1]);
-    // cout << std::numeric_limits<int>::max() << "\\"<< std::numeric_limits<int>::lowest() <<endl;
-    // cout << std::numeric_limits<double>::max() << "\\"<< std::numeric_limits<double>::lowest() <<endl;
-    // double i=1e30;
-    // cout << static_cast<int>(i) <<endl;
+    if (ac != 2)
+    {
+        conv._guide();
+        return (1);
+    }
+    conv.parsing(av[1]);
+    conv.display();
 }
