37b664fb975211a3d7302fa516e49ef4ed15f41c
lamvii <ael-idri@e2r8p11.1337.ma>
2022-09-25 11:16:13 +0100

    fix arry [] for const keyword

--- cpp_Module07/ex02/Array.hpp
+++ cpp_Module07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-idri <ael-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 17:23:38 by ael-idri          #+#    #+#             */
-/*   Updated: 2022/09/19 23:54:06 by ael-idri         ###   ########.fr       */
+/*   Updated: 2022/09/22 17:47:41 by ael-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,9 @@ public:
 	Array & operator =(const Array&);
 	~Array();
 	T& operator [](unsigned int );
+	const T & operator [](unsigned int ) const;
 	unsigned int size() const;
 };
-
 
 template <typename T>
 Array<T>::Array(): arr(new T[0]()), _size(0)
@@ -56,6 +56,7 @@ Array<T>::Array(unsigned int n) : arr(new T[n]()) , _size(n)
 template <typename T>
 Array<T>::Array(const Array&copy)
 {
+	this->arr = new T[copy.size()]();
 	this->_size = copy._size;
 	for (unsigned int i = 0; i < copy.size() ; i++)
 		this->arr[i] = copy.arr[i];
@@ -66,7 +67,8 @@ Array<T> & Array<T>::operator =(const Array&other)
 {
 	if (this == &other)
 		return (*this);
-	this->~Array();
+	delete [] this->arr;
+	this->arr = new T[other.size()]();
 	this->_size = other._size;
 	for (int i = 0; i < other.size() ; i++)
 		this->arr[i] = other.arr[i];
@@ -81,6 +83,15 @@ Array<T>::~Array()
 
 template <typename T>
 T& Array<T>::operator [](unsigned int n)
+{
+	if (n >= this->size() || n < 0)
+		throw MyException("Index is out of bounds");
+	else
+		return (arr[n]);
+}
+
+template <typename T>
+T const & Array<T>::operator [](unsigned int n) const
 {
 	if (n >= this->size() || n < 0)
 		throw MyException("Index is out of bounds");
